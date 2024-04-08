e5d0e55809550fb3112a7a4e9ba03fff1350e766
Angyr-ool <dagbazhyk.a.s@gmail.com>
2024-04-08 04:26:51 +0300

    refactoring

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -4,18 +4,18 @@ namespace UnitTests;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestMethod_1()
     {
         Assert.AreEqual(1, 1);
     }
     [TestMethod]
-    public void TestMethod2()
+    public void TestMethod_2()
     {
         Assert.AreEqual(2, 2);
     }
     [TestMethod]
-    public void TestMethod3()
+    public void TestMethod_3()
     {
-        Assert.AreEqual(3, 3);
+        Assert.AreEqual(3, 0);
     }
 }
